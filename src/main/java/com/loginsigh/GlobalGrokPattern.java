fd1e7871108f86f4d0bae803a594bd61921a3d4f
kira <529280602@qq.com>
2016-11-25 18:18:13 +0800

    clean code by sonar suggest

--- src/main/java/com/loginsigh/GlobalGrokPattern.java
+++ src/main/java/com/loginsigh/GlobalGrokPattern.java
@@ -6,5 +6,5 @@ import oi.thekraken.grok.api.Grok;
  * Created by kira on 16/11/24.
  */
 public class GlobalGrokPattern {
-    public final static Grok globalGrokPattern = Grok.EMPTY;
+    public static final Grok globalGrokPattern = Grok.EMPTY;
 }
