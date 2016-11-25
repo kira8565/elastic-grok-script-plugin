db4ca5ed89e19834447a8b94570d056a37f3e0e7
kira <529280602@qq.com>
2016-11-25 18:07:34 +0800

    make globalGrokPattern final

--- src/main/java/com/loginsigh/GlobalGrokPattern.java
+++ src/main/java/com/loginsigh/GlobalGrokPattern.java
@@ -6,5 +6,5 @@ import oi.thekraken.grok.api.Grok;
  * Created by kira on 16/11/24.
  */
 public class GlobalGrokPattern {
-    public static Grok globalGrokPattern = Grok.EMPTY;
+    public final static Grok globalGrokPattern = Grok.EMPTY;
 }
